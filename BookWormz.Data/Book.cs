008cecd5b8f732e025897aeadedd18bc0bc7e826
seanqiusd <seanqiusoftdev@gmail.com>
2020-09-03 14:29:17 -0400

    added attributes to book class no migrations enabled yet

--- BookWormz.Data/Book.cs
+++ BookWormz.Data/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,16 @@ namespace BookWormz.Data
 {
     public class Book
     {
+        [Required]
         public string ISBN { get; set; }
+
+        [Required]
         public string BookTitle { get; set; }
+
+        [Required]
         public string AuthorFirstName { get; set; }
+
+        [Required]
         public string AuthorLastName { get; set; }
         public string AuthorFullName
         {
@@ -19,9 +27,14 @@ namespace BookWormz.Data
                 return $"{AuthorFirstName} {AuthorLastName}";
             }
         }
+
+        [Required]
         public BookGenre GenreOfBook { get; set; } //many eneums test
+
+        [Required]
         public string Description { get; set; }
-        public int NumberAvaiable { get; set; }
+
+        public int NumberAvailable { get; set; }
 
     }
     public enum BookGenre {Fantasy, Adventure, Romance, Contemporary, Dystopian, Mystery, Horror, Thriller, Paranormal, HistoricalFiction, ScienceFiction, Memoir, Cooking, Art, SelfHelp, Development, Motivational, Health, History, Travel, Guide, FamiliesandRelationships, Humor, Children }
