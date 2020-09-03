5cacccf94d7b83024742707de2f71d3b44ce013c
Hustin Jeffers <hj.paul7@gmail.com>
2020-09-03 13:47:09 -0400

    exchange classes

---
+++ BookWormz.Models/ExchangeCreate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWormz.Models
+{
+    public class ExchangeCreate
+    {
+        public int Id { get; set; }
+
+        public string BookId { get; set; }
+
+        public DateTime Posted { get; set; }
+
+        public DateTime? SentDate { get; set; }
+
+
+
+    }
+}
---
+++ BookWormz.Models/ExchangeListItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWormz.Models
+{
+    public class ExchangeListItem
+    {
+        [Display(Name = "Exchange ID")]
+        public int Id { get; set; }
+
+        [Display(Name = "Book ID")]
+        public string BookId { get; set; }
+
+        [Display(Name = "Date Posted")]
+        public DateTime Posted { get; set; }
+
+        [Display(Name = "Sent Date")]
+        public DateTime? SentDate { get; set; }
+    }
+}
