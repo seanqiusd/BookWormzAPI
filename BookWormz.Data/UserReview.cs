60b992f6f1ef5864cb56884db8b86db9e7028a32
Spawlak93 <63370588+Spawlak93@users.noreply.github.com>
2020-09-03 14:20:52 -0400

    added UserReview Class

---
+++ BookWormz.Data/UserReview.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWormz.Data
+{
+    public class UserReview
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Reviewer))]
+        public Guid UserId { get; set; }
+        public virtual ApplicationUser Reviewer { get; set; }
+
+        [ForeignKey(nameof(Exchange))]
+        public int ExchangeId { get; set; }
+        public virtual Exchange Exchange { get; set; }
+
+        [Required]
+
+        public double ExchangeRating { get; set; }
+    }
+}
