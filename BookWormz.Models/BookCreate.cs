d1a0540f1b5a3cc7c86ea90d8ba4b7a3772caf21
seanqiusd <seanqiusoftdev@gmail.com>
2020-09-04 12:18:36 -0400

    seantwo branch added bookcreate bookservice and bookcontroller

---
+++ BookWormz.Models/BookCreate.cs
@@ -0,0 +1,36 @@
+﻿using BookWormz.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWormz.Models
+{
+    public class BookCreate
+    {
+       
+        [Required]
+        public string ISBN { get; set; } 
+
+        [Required]
+        [MinLength(1, ErrorMessage ="At least 1 character")]
+        public string BookTitle { get; set; }
+
+       [Required]
+        public string AuthorFistName { get; set; }
+
+       [Required]
+        public string AuthorLastName { get; set; }
+
+        [Required]
+        public BookGenre GenreOfBook { get; set; }
+
+       [Required]
+       [MinLength(15, ErrorMessage ="At least 15 characters")]
+        public string Description { get; set; }
+
+
+    }
+}
