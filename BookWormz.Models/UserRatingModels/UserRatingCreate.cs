2fadf5470684ad3f274a348afbd926940a3c41e8
Spawlak93 <63370588+Spawlak93@users.noreply.github.com>
2020-09-04 09:48:12 -0400

    added UserRating Models

---
+++ BookWormz.Models/UserRatingModels/UserRatingCreate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWormz.Models.UserRatingModels
+{
+    class UserRatingCreate
+    {
+        [Required]
+        [Display(Name = "Exchange Id Number")]
+        public int ExchangeId { get; set; }
+
+        [Required]
+        [Display(Name = "User Id to be rated")]
+        public string UserId { get; set; }
+
+        [Required]
+        [Display(Name = "Exchange Rating score")]
+        [Range(1,10)]
+        public double ExchangeRating { get; set; }
+    }
+}
