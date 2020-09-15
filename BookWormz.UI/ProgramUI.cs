91fde8f204ca8e8177c53a6917a7016f217d5e6d
Hustin Jeffers <hj.paul7@gmail.com>
2020-09-15 16:45:35 -0400

    handlers fix

--- BookWormz.UI/ProgramUI.cs
+++ BookWormz.UI/ProgramUI.cs
@@ -843,8 +843,6 @@ namespace BookWormz.UI
             Dictionary<string, string> rating = new Dictionary<string, string>();
 
             string exchangeIdInput = Console.ReadLine();
-            rating.Add("ExchangeId", exchangeIdInput);
-
             Console.Write("Exchange Rating: ");
             string exchangeRatingInput = Console.ReadLine();
             rating.Add("ExchangeRating", exchangeRatingInput);
